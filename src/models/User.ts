756d17bad44f952975c0c1a27a45edfd6c163cba
Justin <j.hebenstreit25@gmail.com>
2024-10-16 16:30:13 -0500

    added file and almost finished Thought.ts

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,43 +1,38 @@
-import { Schema, Types, model, type Document } from 'mongoose';
+import { Schema, Types, model, type Document } from "mongoose";
 
 interface IUser extends Document {
-    username: string,
-    email: string,
-    thoughts: Types.ObjectId[],
-    friends: Types.ObjectId[]
+  username: string;
+  email: string;
+  thoughts: Types.ObjectId[];
+  friends: Types.ObjectId[];
 }
 
-
-
-const UserSchema = new Schema<IUser>(
+const UserSchema = new Schema<IUser>({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trimmed: true,
+  },
+  email: {
+    type: String,
+    runique: true,
+    match: [/.+@.+\..+/, "Please enter a valid e-mail address"],
+  },
+  thoughts: [
     {
-        username: {
-            type: String,
-            required: true,
-            unique: true,
-            trimmed: true,
-        },
-        email: {
-            type: String,
-            runique: true,
-            match: [/.+@.+\..+/, 'Please enter a valid e-mail address'],
-        },
-        thoughts: [
-            {
-                type: Schema.Types.ObjectId,
-                ref: 'Thought',
-            },
-        ],
-        friends: [
-            {
-                type: Schema.Types.ObjectId,
-                ref: 'User',
-            },
-        ],
+      type: Schema.Types.ObjectId,
+      ref: "Thought",
     },
-);
+  ],
+  friends: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
+});
 
-
-const Users = model('User', UserSchema);
+const Users = model("User", UserSchema);
 
 export default Users;
